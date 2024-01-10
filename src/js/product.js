5d5c9fae027034634f03e95603caf87c32956843
ashleyka <ashleykamujoma@gmail.com>
2024-01-10 12:14:52 +0200

    fix add to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const currentcart = getLocalStorage("so-cart") || [] ;
+  currentcart.push(product);
+  setLocalStorage("so-cart", currentcart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
