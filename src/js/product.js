5b8ce7ca4665efa06d4ac0a12b46ec216609bf61
Sebastiao Matusse <matussevalete@gmail.com>
2024-01-18 13:35:09 -0700

    urls parameters added to the project and able to get them consoling

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,8 @@
-import { setLocalStorage, getLocalStorage} from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -18,3 +13,8 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+const productId = getParam("product");
+console.log(productId);
+
+console.log(dataSource.findProductById(productId));
