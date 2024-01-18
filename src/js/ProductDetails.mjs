f5b18fe37caa399a65e71ed92d3a5a0791824d4f
Sebastiao Matusse <matussevalete@gmail.com>
2024-01-18 15:22:42 -0700

    refined productDetails module with instructor's solution

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,3 +1,5 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -6,21 +8,25 @@ export default class ProductDetails {
   }
 
   async init() {
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
+    // once we have the product details we can render out the HTML
     this.renderProductDetails();
-    // document
-    //   .getElementById("addToCart")
-    //   .addEventListener("click", this.addToCartHandler.bind(this));
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
   }
 
-  // addProductToCart(product) {
-  //   const cartItems = getLocalStorage("so-cart") || [];
-  //   cartItems.push(product);
-  //   setLocalStorage("so-cart", cartItems);
-  // }
+  addProductToCart() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
 
   renderProductDetails() {
-    let parentElement = document.querySelector(".divider");
+    let parentElement = document.querySelector("main");
 
     let childTemplateElement = `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
     <h2 class="divider">${this.product.NameWithoutBrand}</h2>
