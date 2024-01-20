27fdad754233141393723194a2c277e0db3302f5
indlukula <84289216+indlukula@users.noreply.github.com>
2024-01-20 21:24:52 +0200

    created new branch for individual2

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,26 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+// const dataSource = new ProductData("tents");
 
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
+
+// const productId = getParam("product");
+// console.log(productId);
+
+// console.log(dataSource.findProductById(productId));
+
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-let productList = getLocalStorage("so-cart");
-let itemList = Array.from(productList);
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
