f63f0a250ea1a19746c56e1a2281b95087a1b160
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-01-20 23:48:09 +0800

    prettier hit it

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductListing.mjs";
 
-
 const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list")
+const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
-
 
 listing.init();
