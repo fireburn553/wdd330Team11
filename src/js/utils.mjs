08393e27d78022810a1b4a59d9c7b49119098ea4
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-01-20 23:13:54 +0800

    individual activity 2 done

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,13 @@ export function getParam(param) {
   const product = urlParams.get(param);
   return product;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false){
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+  parentElement.innerHTML = "";
+  }
+    
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+  
+}
