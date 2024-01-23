28d1963e02fb0a2fe3b6c63fe05c7d7ffcd98dec
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-01-23 14:50:51 +0800

    Fix the cart badge counter and fix the format and error

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,7 @@
-import { getLocalStorage, setCartCount, getCartCount} from "./utils.mjs";
+import { getLocalStorage, getCartCount } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
-  setCartCount(cartItems.length);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   document.querySelector(".cart-count").innerText = getCartCount().toString();
